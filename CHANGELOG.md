919c26bd24fe736f795a0abc78af63a78f0e9e39
moresales <luodong@moresales.cn>
2021-05-29 16:46:47 +0800

    export sql util

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.0.2
+## 1.0.2|3
 
 - Add `sql.dart` which is SQL builder tool.
 
--- lib/dartboot_db.dart
+++ lib/dartboot_db.dart
@@ -2,3 +2,4 @@ library dartboot_db;
 
 export 'src/model_utils.dart';
 export 'src/error/db_error.dart';
+export 'src/util/sql.dart';
